e393c0812bdae5becc105268edc04ea52ca90a58
Jay Patel <patel.jay@berkeley.edu>
2013-11-15 17:50:11 -0800

    fixed zipcode script, fixed merge issues in html

--- src/server/opinion/scripts/populate_zipcodes.py
+++ src/server/opinion/scripts/populate_zipcodes.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 import environ
 import os
+import csv
 from opinion.opinion_core.models import *
 
 """
 The fields of the file look like this:
-"zip","type","primary_city","acceptable_cities","unacceptable_cities","state","county","timezone","area_codes","latitude","longitude","world_region","country","decommissioned","estimated_population","notes"
+zip,type,primary_city,acceptable_cities,unacceptable_cities,state,county,timezone,area_codes,latitude,longitude,world_region,country,decommissioned,estimated_population,notes
 """
 
-with open('/Users/jaypatel/cafe/doc/zip_code_database.csv') as zipcode_db:
-    for i, line in enumerate(zipcode_db):
-        if i == 0: #header row
-            continue
-        else:
-            line = line.split(",")
-            if 'CA' in line[5]:
-                code, city, state = line[0], line[2], line[5]
-                z = ZipCode(code=code, city=city, state=state)
-                z.save()
+fname = '../../../doc/zip_code_database.csv'
+raw_input("Using zipcode db located at: " \
+          + os.path.abspath(fname) + ", ok? [y]")
+
+for i, line in enumerate(csv.reader(open(fname))):
+    if i == 0: # header row
+        continue
+    else:
+        if line[5] in ['CA']: # add more states here if wanted
+            code, city, state = line[0], line[2], line[5]
+            z = ZipCode(code=code, city=city, state=state)
+            z.save()
                 
             
         
