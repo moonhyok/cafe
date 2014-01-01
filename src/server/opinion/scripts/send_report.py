27ee5020b8a980521fb05d6e3e86c54cfc934cb8
root <root@citizenreportcard.org>
2014-01-01 07:43:33 -0800

    added the html send_report, its still a little messy

--- src/server/opinion/scripts/send_report.py
+++ src/server/opinion/scripts/send_report.py
@@ -32,15 +32,24 @@ def create_table_row_user(user):
     user_data_login = UserData.objects.filter(user = user, key = 'first_login')
     if len(user_data_login) > 0:
         time_joined_display = DateFormat(datetime.datetime.fromtimestamp(float(user_data_login[0].value))).format('h:i:s A')
-    username_display = user.id
+    username_display = user.username
     if user.email != '':
 		email_display = '--'
     else:
 		email_display = '--'
     table_row += time_joined_display + '\t' + username_display + '\t' + email_display + '\t' + str(score)
-    if user_demographics != None:
-        table_row += display_demographic(table_row, decode_to_unicode(user_demographics.location))
-        table_row += display_demographic(table_row, decode_to_unicode(user_demographics.heard_about))
+    # if user_demographics != None:
+    #     table_row += display_demographic(table_row, decode_to_unicode(user_demographics.location))
+    #     table_row += display_demographic(table_row, decode_to_unicode(user_demographics.heard_about))
+    zip_qs = ZipCodeLog.objects.filter(user=user)
+    zip_code = str(zip_qs[0].location.city) if zip_qs else ''
+    table_row += display_demographic(table_row, zip_code)
+
+    ## Did they leave a comment?
+    # TODO: make sure it is the current discussion questions
+    commented = DiscussionComment.objects.filter(user = user).exists()
+    table_row += "\tyes" if commented else "\tno"
+    
     return table_row
 		
 url_list = string.split(URL_ROOT,'/')
@@ -48,11 +57,11 @@ title = " " + url_list[len(url_list) - 1] + " "
 
 # Subject
 df_now = DateFormat(datetime.datetime.now())
-report_subject = 'Citizen Report Card (on citizenreportcard.org) Acitivity Digest for ' + df_now.format('l, F jS, Y')
+report_subject = '[California Report Card] Activity Digest for ' + df_now.format('l, F jS, Y')
 
 # From
 #report_from = Settings.objects.string('DEFAULT_FROM_EMAIL')
-report_from = 'messages@citizenreportcard.org'
+report_from = 'messages@californiareportcard.org'
 
 # Recipients
 if EMAIL_RECIPIENTS:
@@ -62,13 +71,21 @@ if EMAIL_RECIPIENTS:
 report_body = ''
 
 ## Header
-report_header = report_subject + '\n'
-report_body += report_header
+report_header = report_subject
+report_body += "<h2>%s</h2>" % report_header
+report_body += "californiareportcard.org"
 
-## Feedback
+## Quick stats
+report_body += "<h3>Number of users so far: %s</h3>" % User.objects.filter(id__gte = 310).count()
+
+# TODO: add number of users who gave us their email?
+
+# We aren't collecting feedback now (12/26)
+"""
+### Feedback
 feedbacks = Feedback.objects.filter(created__gte = datetime.date.today() - datetime.timedelta(days=1))
-feedback_display = '\n'
-feedback_display += 'Feedback:\n'
+feedback_display = ''
+feedback_display += '<h3>Feedback:</h3>'
 for feedback in feedbacks:
     feedback_line = ''
     if feedback.user != None:
@@ -78,6 +95,7 @@ for feedback in feedbacks:
     feedback_line += ' at ' + DateFormat(feedback.created).format('h:i:s A') + ': ' + decode_to_unicode(feedback.feedback)
     feedback_display += feedback_line + '\n'
 report_body += feedback_display
+"""
 
 ## New users
 table_body = ''
@@ -90,23 +108,30 @@ if Settings.objects.boolean('USE_ENTRY_CODES'):
 else:
 	users = User.objects.filter(date_joined__gte = datetime.datetime.now() - datetime.timedelta(days=1))
 
-table_body += '\n' + 'New users that registered today:\n' + 'Format: Time Registered (PST), Username, Email, Reviewer Score, Location, Heard From (tab delimited)'
+table_body += '<h3>New users that registered today:</h3>'
+
+table_body += '<table border="1">'
+table_body += "<tr><td>Time Registered (PST)</td><td>User ID</td><td>Email</td><td>Reviewer Score</td><td>Location</td><td>Left a comment?</td></tr>"
+
 for user in users:
-	table_row = ''
-	table_row = create_table_row_user(user)
-	table_body += '\n' + table_row 
+	table_row = '<tr>'
+        row_data = create_table_row_user(user).split("\t")
+        print row_data
+        row_data = map(lambda x: "<td>" + x + "</td>", row_data)
+	table_row += "".join(row_data)
+        table_row += "</tr>"
+        table_body += table_row
+table_body += "</table>"
 report_body += table_body
 
 ## Daily Statistics
-daily_statistics = '\n\n'
-daily_statistics += 'Total new users: ' + str(len(users))
-daily_statistics += '\n'
+daily_statistics = ""
+daily_statistics += '<h3>Total new users: ' + str(len(users)) + '</h3>'
 report_body += daily_statistics
 
 ## Top comments
 OS_ID = 1
-report_body += "\n"
-report_body += "Top comments from the past week:\n" 
+report_body += "<h3>Top comments from the past week:</h3>" 
 
 os = OpinionSpace.objects.get(pk = OS_ID)
 discussion_statement_objects = os.discussion_statements.filter(is_current = True)
@@ -118,15 +143,24 @@ for comment in comments:
 		count+=1
 
 ## Flagged comments
-report_body += "\n"
-report_body += "Flagged comments:\n" 
+report_body += ""
+report_body += "<h3>Flagged comments:</h3>" 
 
 flagged = FlaggedComment.objects.all().order_by('comment')
 printed = []
 for fcomment in flagged:
+    if fcomment.created.date() >= datetime.date.today() - datetime.timedelta(days=7):
 	if fcomment.comment.discussion_statement == discussion_statement_objects[0]:
 		if fcomment.comment not in printed and not_admin_approved(fcomment.comment):
-			report_body += 'Username: ' + fcomment.comment.user.username + '\nEmail: ' + fcomment.comment.user.email + "\nComment: " + decode_to_unicode(fcomment.comment.comment) + '\n'
+			report_body += 'Username: ' + fcomment.comment.user.username + '\nEmail: ' \
+                            + fcomment.comment.user.email + "\nComment: " + decode_to_unicode(fcomment.comment.comment) + '\n\n'
 			printed.append(fcomment.comment)
 
-send_mail(report_subject, report_body, report_from, report_recipients)
+#send_mail(report_subject, report_body, report_from, report_recipients)
+
+from django.core.mail import EmailMultiAlternatives
+msg = EmailMultiAlternatives(report_subject, report_body, report_from, report_recipients)
+html_content = report_body.replace("\n", "<br>")
+msg.attach_alternative(html_content, "text/html")
+msg.send()
+
