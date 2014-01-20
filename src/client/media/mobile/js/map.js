c9e148f4c05e1dd3423af528a54e9140def753b4
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-01-20 11:59:22 -0800

    add label and border

--- src/client/media/mobile/js/map.js
+++ src/client/media/mobile/js/map.js
@@ -61,13 +61,30 @@
 			return {
 				weight: 1,
 				opacity: 1,
-				color: 'white',
-				dashArray: '3',
+				color: borderColor(feature),
+				dashArray: borderDash(feature),
 				fillOpacity: 0.7,
 				fillColor: getColor(feature.properties.s1)
 			};
 		}
-
+        function borderColor(feature){
+					   if(feature.properties.NAME=="Participants Outside California"){
+					     return '#170e03';
+					    }
+					   else{
+						 return 'white';
+						 }
+		}
+		function borderDash(feature){
+			if(feature.properties.NAME=="Participants Outside California"){
+					     return '';
+					    }
+					   else{
+						 return '3';
+						 }
+		}
+					   
+					   
 		function highlightFeature(e) {
 			var layer = e.target;
 
@@ -142,6 +159,56 @@
 
 		legend.addTo(map); 
 
+//for label
+    L.LabelOverlay = L.Class.extend({
+        initialize: function(/*LatLng*/ latLng, /*String*/ label, options) {
+            this._latlng = latLng;
+            this._label = label;
+            L.Util.setOptions(this, options);
+        },
+        options: {
+            offset: new L.Point(0, 2)
+        },
+        onAdd: function(map) {
+            this._map = map;
+            if (!this._container) {
+                this._initLayout();
+            }
+            map.getPanes().overlayPane.appendChild(this._container);
+            this._container.innerHTML = this._label;
+            map.on('viewreset', this._reset, this);
+            this._reset();
+        },
+        onRemove: function(map) {
+            map.getPanes().overlayPane.removeChild(this._container);
+            map.off('viewreset', this._reset, this);
+        },
+        _reset: function() {
+            var pos = this._map.latLngToLayerPoint(this._latlng);
+            var op = new L.Point(pos.x + this.options.offset.x, pos.y - this.options.offset.y);
+            L.DomUtil.setPosition(this._container, op);
+        },
+        _initLayout: function() {
+            this._container = L.DomUtil.create('div', 'leaflet-label-overlay');
+        }
+    });   
+
+var labelLocation1 = new L.LatLng(40.2, -118.8);
+var labelLocation2 = new L.LatLng(39.9, -118.8);
+var labelTitle1 = new L.LabelOverlay(labelLocation1, 'Outside');
+    map.addLayer(labelTitle1);
+    
+var labelTitle2 = new L.LabelOverlay(labelLocation2, 'California');
+    map.addLayer(labelTitle2);
+    
+    map.on('movestart', function () {
+        map.removeLayer(labelTitle1);
+        map.removeLayer(labelTitle2);
+    });
+    map.on('moveend', function () {
+        map.addLayer(labelTitle1);
+        map.addLayer(labelTitle2);
+    });
 
 
 function changeData(sel)
@@ -152,8 +219,8 @@ function changeData(sel)
 	    return {
 				weight: 1,
 				opacity: 1,
-				color: 'white',
-				dashArray: '3',
+				color: borderColor(feature),
+				dashArray: borderDash(feature),
 				fillOpacity: 0.7,
 				fillColor: getColor(feature.properties[value])
 			};
