d9faec4a1ce04d76035c1a5809a6de0821a021c3
root <root@californiareportcard.org>
2014-01-26 21:37:45 -0800

    A few update

--- src/server/opinion/scripts/temp_script.py
+++ src/server/opinion/scripts/temp_script.py
@@ -3,11 +3,5 @@ import environ
 from opinion.opinion_core.models import *
 import numpy as np
 
-comments = DiscussionComment.objects.filter(is_current=True)
-print comments.count()
-count = 0
-for c in comments:
-	if CommentRating.objects.filter(comment = c,is_current=True).count() >= 5:
-		count = count + 1
-		print CommentRating.objects.filter(comment = c).order_by("created")[4].created - c.created 
-print count
+c = User.objects.filter(is_active = False).count()
+print c
