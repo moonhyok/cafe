4267efd9da2cb40b78ffaf53914b7d8b515c3539
root <root@californiareportcard.org>
2014-01-27 21:27:03 -0800

    Pushing server changes

--- src/server/opinion/scripts/recalculate_median_grades.py
+++ src/server/opinion/scripts/recalculate_median_grades.py
@@ -9,15 +9,17 @@ skip_begin_date=datetime.datetime(2014,1,9,0,0,0,0)
 for s in OpinionSpaceStatement.objects.all():
     ratings = UserRating.objects.filter(is_current=True,opinion_space_statement = s, user__in = active_users)
     skip_logs = LogUserEvents.objects.filter(is_visitor=True, log_type=11,details__contains='slider_set',created__gte = skip_begin_date)
-    users_to_skip = []
+    users_with_grades = []
     for skip_log in skip_logs:
         slider_set_args = skip_log.details[10:].lstrip()
-        visitor = Visitor.objects.filter(id = skip_log.logger_id)[0]
-        if visitor.user != None and visitor.user.is_active and int(slider_set_args[0]) == s.id:
-            users_to_skip.append(visitor.user)
+        visitor = Visitor.objects.filter(id = skip_log.logger_id)
+	if len(visitor) > 0:
+          visitor = visitor[0]
+          if visitor.user != None and visitor.user.is_active and int(slider_set_args[0]) == s.id:
+            users_with_grades.append(visitor.user)
 
     cache = StatementMedians.objects.filter(statement = s)
-    value = np.median(ratings.exclude(user__in = users_to_skip).values_list('rating'))
+    value = np.median(ratings.filter(user__in = users_with_grades).values_list('rating'))
     if cache.count() == 0:
         StatementMedians(statement = s, rating = value).save()
     else:
