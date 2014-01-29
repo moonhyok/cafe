49708f01f81d61484ffc6b7085f6995f940c15b0
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-01-29 14:33:42 -0800

    remove base layer in map

--- src/client/media/mobile/js/map.js
+++ src/client/media/mobile/js/map.js
@@ -2,11 +2,11 @@
 
 		var map = L.map('map').setView([37.9, -117.8], 5);
         
-		var cloudmade = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
+		/*var cloudmade = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
 			attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
 			key: 'BC9A493B41014CAABB98F0471D759707',
 			styleId: 22677
-		}).addTo(map);
+		}).addTo(map);*/
 
 
 		// control that shows state info on hover
