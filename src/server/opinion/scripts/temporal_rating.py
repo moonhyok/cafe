9277bab0bde9e55902f5ebf49b32c200a352cd5b
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-02-04 15:05:47 -0800

    Update temporal_rating.py

--- src/server/opinion/scripts/temporal_rating.py
+++ src/server/opinion/scripts/temporal_rating.py
@@ -95,14 +95,14 @@ def rating_change_difference():
 					if len(s_log_rating)>1: # not skip user change rating
 						grade_initial=median_index(1-float(s_log_rating[len(s_log_rating)-1].details.split()[2])) #get initial grade
 						grade_final=median_index(1-float(s_log_rating[0].details.split()[2]))
-						grade_diff=grade_final-grade_initial
+						grade_diff=-(grade_final-grade_initial)
 						rate_diff[grade_diff+12]=rate_diff[grade_diff+12]+1
 				else:
 					if len(s_log_rating)>1:
-						if s_log_skip[0].created<s_log_rating[0].created: # final decision is rating
+						if s_log_skip[0].created<s_log_rating[0].created: # final rating
 							grade_initial=median_index(1-float(s_log_rating[len(s_log_rating)-1].details.split()[2]))
 							grade_final=median_index(1-float(s_log_rating[0].details.split()[2]))
-							grade_diff=grade_final-grade_initial
+							grade_diff=-(grade_final-grade_initial)
 							rate_diff[grade_diff+12]=rate_diff[grade_diff+12]+1
 		rate_diff_record.append(rate_diff) 
 	for record in rate_diff_record:
