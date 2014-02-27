f7e91f499ae21df4661b9930e8888adaf2e99ec3
root <root@californiareportcard.org>
2014-02-27 15:23:58 -0800

    Python hook for user totals

---
+++ src/server/opinion/scripts/recalculate_total_users.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import environ
+from opinion.opinion_core.models import *
+import numpy as np
+import random
+import os
+
+data =  int(os.popen("./total_users_apache_logs.sh").readlines()[0].rstrip())
+su = User.objects.filter(id=1)[0]
+udo  = UserData.objects.filter(user=su,key='user_count')
+if udo.count() == 0:
+	udo =  UserData(user=su,key='total_count',value=data)
+	udo.save()
+else:
+	udo[0].value = data
+	udo[0].save()
+
