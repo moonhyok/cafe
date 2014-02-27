f41a7b9bfbec408bf189f6d65990a7a1a9666369
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-02-26 23:12:36 -0800

    Update send_grade_ready_email.py

--- src/server/opinion/scripts/send_grade_ready_email.py
+++ src/server/opinion/scripts/send_grade_ready_email.py
@@ -17,17 +17,17 @@ for user in user_active:
        comment=DiscussionComment.objects.filter(user = user,is_current=True)
        if len(comment)>0:
           user_email.append(user)
-          
+
 print len(user_email)
 
 for user in user_email:
     entrycode=EntryCode.objects.filter(username=user.username,first_login=False)
-    print len(entrycode)
     if len(entrycode)>0:
         subject = "Your grades are ready to view at the California Report Card!"
         received = 2*CommentAgreement.objects.filter(comment__in = DiscussionComment.objects.filter(user = user),is_current=True).count()
         email_list = [user.email]
         comment=DiscussionComment.objects.filter(user = user,is_current=True)
+        print User.objects.filter(date_joined__gte=launch_day).count()
         message = render_to_string('registration/crc_grade_ready.txt',
                                   {'entrycode': entrycode[0].code,
                                    'received': received/2,
@@ -36,7 +36,6 @@ for user in user_email:
                                     })
         try:
            #send_mail(subject, message, Settings.objects.string('DEFAULT_FROM_EMAIL'), email_list)
-           
            time.sleep(0.3)
         except:
            pass
