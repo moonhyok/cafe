2261675bbf956d1a6a71b04e469e13c93904a41a
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-03-16 14:52:57 -0700

    Update send_event_invitation.py

--- src/server/opinion/scripts/send_event_invitation.py
+++ src/server/opinion/scripts/send_event_invitation.py
@@ -11,7 +11,7 @@ import csv
 import hashlib
 
 user_active=User.objects.filter(is_active=True)
-duplicated_email=['hunallen@gmail.com','patel.jay@berkeley.edu','ccrittenden@berkeley.edu','nonnecke@citris-uc.org','nonnecke@berkeley.edu','tanja.aitamurto@gmail.com','sanjaykrishn@gmail.com','goldberg@berkeley.edu']
+duplicated_email=['hunallen@gmail.com','patel.jay@berkeley.edu','ccrittenden@berkeley.edu','nonnecke@citris-uc.org','nonnecke@berkeley.edu','tanja.aitamurto@gmail.com']
 user_email=[]
 
 for user in user_active:
@@ -30,6 +30,16 @@ for email in duplicated_email:
           comment=DiscussionComment.objects.filter(user = user,is_current=True)
           if len(comment)>0:
              user_email.append(user)
+
+email_ken='goldberg@berkeley.edu'
+entrycode=hashlib.sha224(email_ken).hexdigest()[0:7]   
+candidate=EntryCode.objects.filter(code__exact=entrycode).order_by('id')
+if len(candidate)>0:
+   user = User.objects.filter(username__exact=candidate[len(candidate)-2].username).order_by('id')
+   if len(user)>0:
+      user= user[len(user)-1]
+      user_email.append(user)
+
 
 for user in user_email:
     entrycode=EntryCode.objects.filter(username=user.username)
