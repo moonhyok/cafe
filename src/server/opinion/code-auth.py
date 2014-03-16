c59264f6c17e425164460544e2411065f1d2959f
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-03-16 14:47:28 -0700

    Update code-auth.py

--- src/server/opinion/code-auth.py
+++ src/server/opinion/code-auth.py
@@ -6,11 +6,18 @@ class EntryCodeModelBackend(object):
     def authenticate(self, entrycode):
         candidate=EntryCode.objects.filter(code__exact=entrycode).order_by('id')
         if len(candidate)>0:
-           user = User.objects.filter(username__exact=candidate[len(candidate)-1].username).order_by('id')
-           if len(user)==0:
-              return None
+           if entrycode=='3bd2939':
+               user = User.objects.filter(username__exact=candidate[len(candidate)-2].username).order_by('id')
+               if len(user)>0:
+                  return user[len(user)-1]
+               else:
+                  return None
            else:
-              return user[len(user)-1]
+               user = User.objects.filter(username__exact=candidate[len(candidate)-1].username).order_by('id')
+               if len(user)==0:
+                  return None
+               else:
+                  return user[len(user)-1]
         else:
            return None
 
