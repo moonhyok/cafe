49b2a3a805acc4eb3572a8ef1eeede8c79204992
Sanjay Krishnan <sanjaykrishn@gmail.com>
2014-05-01 16:28:03 -0700

    Updated with a fix

--- src/client/media/mobile/js/crc-transparent-slider.js
+++ src/client/media/mobile/js/crc-transparent-slider.js
@@ -42,7 +42,7 @@ $(".slider-grade-bubble").on("click",function(e){
 });
 
 $(".slider-grade-bubble").on("touchstart",function(e){ 
-	$(this).parent().children(".slider-grade-bubble").css("opacity","0.45"); 
+	$(this).parent().children(".slider-grade-bubble").css("opacity","1.0"); 
 	$(this).parent().children(".slider-grade-bubble").css("border","2px solid #FFFFFF");
 	$(this).parent().children(".slider-grade-bubble").css("color","#FFFFFF");
 
