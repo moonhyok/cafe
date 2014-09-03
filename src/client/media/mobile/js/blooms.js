63610ccf3efd8fed5ece76d8e50b1e58f5ed40b5
Sanjay Krishnan <sanjaykrishn@gmail.com>
2014-09-02 22:43:48 -0700

    Updated

--- src/client/media/mobile/js/blooms.js
+++ src/client/media/mobile/js/blooms.js
@@ -324,7 +324,7 @@ var _blooms = blooms = (function($, d3, console) {
             top: 0,
             left: 10,
             right: 80,
-            bottom: 50
+            bottom: 70
             };
 
 
