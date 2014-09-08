0883a488d76582a7acacfd0b643179ebfd533d85
Sanjay Krishnan <sanjaykrishn@gmail.com>
2014-09-08 00:22:03 -0700

    Updated

---
+++ src/client/media/mobile/js/translations.js
@@ -0,0 +1,47 @@
+var translations = {
+                    'California Report Card': 
+                        'Califique California',
+                    
+                    'Español': 
+                        'English',
+
+                    'Join others who have contributed to California\'s collective intelligence.':
+                        'Únete a otros que ya han contribuido a la inteligencia colectiva de California.',
+
+                    'Begin':
+                        'Comenzar',
+
+                    'About':
+                        'Ayuda',
+
+                    'Assign Grades':
+                        'Asigne Calificaciones',
+
+                    'Save':
+                        'Guardar',
+
+                    'Question':
+                        'Pregunta',
+
+                    'Enter your zip code to save your grades and join the discussion':
+                        'Ingrese su código postal para guardar sus calificaciones',
+
+                    'Skip':
+                        'Saltar',
+
+                    'Exit Application':
+                        'Salir',
+
+                    'Your email address will not be displayed and we promise not to share your email address with any third parties.':
+                        'Su dirección de correo electrónico no será publicada y además nos comprometemos a no compartirla con terceros.',
+
+                    'Next':
+                        'Próximo',
+
+                    'How important is this issue for the next Report Card?':
+                        '¿Cuán importante es este asunto como para ser incluido en la próxima Report Card?',
+
+                    'Zipcode must be 5 digits':
+                    'El código postal debe tener 5 dígito'
+
+                    }
