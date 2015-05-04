85f278573168853ef8fb25abf93a0a6352003693
Jay Patel <patel.jay@berkeley.edu>
2015-05-04 11:10:22 -0700

    basic style/content changes for uganda/audio

--- src/client/media/mobile/js/blooms.js
+++ src/client/media/mobile/js/blooms.js
@@ -460,6 +460,7 @@ var _blooms = blooms = (function($, d3, console) {
                 var content = '"'+commentData.comment+'"';
                 var contentSpanish = '"'+commentData.spanish_comment+'"';
                 var cid = commentData.cid;
+                $('.rate .listen').attr('sound', cid);
                 window.current_cid = cid;
                 window.current_uid = d.uid;
                 //$('.rate-loading').show();
