fb66dde30d5c42df6a337f5bf30c10a80c4d15b4
Jay Patel <patel.jay@berkeley.edu>
2015-05-24 18:11:25 -0700

    add 6 questions and revise images for st4-6

--- src/server/opinion/setup.sh
+++ src/server/opinion/setup.sh
@@ -2,7 +2,7 @@ rm uganda-crc.db
 python manage.py syncdb --noinput
 #python manage.py createsuperuser --username=admin --email=admin@example.com --noinput
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
-python scripts/create_db_uganda.py 5
+python scripts/create_db_uganda.py 6
 python scripts/populate_db_uganda.py
 python scripts/add_ud_cache_for_ratings.py
 python scripts/recalculate_median_grades.py
