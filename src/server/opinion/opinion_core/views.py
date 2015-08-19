17d17b0b1e127002c3199e1db2d34912dac86fb6
mozhou <mzhou@berkeley.edu>
2015-08-18 21:37:00 -0700

    trial 2

--- src/server/opinion/opinion_core/views.py
+++ src/server/opinion/opinion_core/views.py
@@ -178,14 +178,14 @@ def mobile(request,entry_code=None):
     if external_count.count() > 0:
        num_users = external_count[0].value
     # print str(request.user.is_authenticated()) + " authenticated"
-    start_date = datetime.datetime(year=2015, month=8, day=17,hour=0, minute=0, second=0, microsecond=0)
-    end_date = start_date + datetime.timedelta(weeks=17)
+    #start_date = datetime.datetime(year=2015, month=8, day=17,hour=0, minute=0, second=0, microsecond=0)
+    #end_date = start_date + datetime.timedelta(weeks=17)
    # except:
    #   start_date = os.created
    #   end_date = os.created + datetime.timedelta(weeks=17)
 
-    as_of_date = min(datetime.datetime.today(), end_date)
-    week_num = ((as_of_date - start_date).days / 7) + 1
+    #as_of_date = min(datetime.datetime.today(), end_date)
+    week_num = 1 #((as_of_date - start_date).days / 7) + 1
 
     last_rating = UserRating.objects.filter(is_current = True).order_by('-created')[1]
     current_user = user
