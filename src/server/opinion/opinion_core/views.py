9a43776c147082498fc45d1acb70af516c39e5c2
mozhou <mzhou@berkeley.edu>
2015-08-19 09:10:54 -0700

    Update mcafe stats page

--- src/server/opinion/opinion_core/views.py
+++ src/server/opinion/opinion_core/views.py
@@ -185,7 +185,7 @@ def mobile(request,entry_code=None):
    #   end_date = os.created + datetime.timedelta(weeks=17)
 
     as_of_date = min(datetime.datetime.today(), end_date)
-    week_num = 1 #((as_of_date - start_date).days / 7) + 1
+    week_num = ((as_of_date - start_date).days / 7) + 1
 
     last_rating = UserRating.objects.filter(is_current = True).order_by('-created')[1]
     current_user = user
@@ -387,17 +387,25 @@ def mcafe_stats(request):
     limit = 15
     recent_comments = DiscussionComment.objects.filter(user__in=active_users, is_current=True).order_by('-created')[:limit]
 
-    try:
-      start_date = opinion.settings_local.START_DATE
-      end_date = opinion.settings_local.END_DATE
-    except:
-      start_date = os.created
-      end_date = os.created + datetime.timedelta(weeks=17)
+   # try:
+   #   start_date = opinion.settings_local.START_DATE
+   #   end_date = opinion.settings_local.END_DATE
+   # except:
+   #   start_date = os.created
+   #   end_date = os.created + datetime.timedelta(weeks=17)
 #    start_date = datetime.date(year=2015, month=8, day=1)
 #    end_date = start_date + datetime.timedelta(weeks=17)
+    start_date = datetime.datetime(year=2015, month=8, day=17,hour=0, minute=0, second=0, microsecond=0)
+    end_date = start_date + datetime.timedelta(weeks=17)
+   # except:
+   #   start_date = os.created
+   #   end_date = os.created + datetime.timedelta(weeks=17)
 
-    as_of_date = min(datetime.datetime.today().date(), end_date.date())
-    week_num = ((as_of_date - start_date.date()).days / 7) + 1
+    as_of_date = min(datetime.datetime.today(), end_date)
+    week_num = ((as_of_date - start_date).days / 7) + 1
+
+   # as_of_date = min(datetime.datetime.today().date(), end_date.date())
+   # week_num = ((as_of_date - start_date.date()).days / 7) + 1
 
     date_threshold = datetime.datetime.today() - datetime.timedelta(days=10)
     comments = DiscussionComment.objects.filter(discussion_statement=disc_stmt, created__gte=date_threshold)
