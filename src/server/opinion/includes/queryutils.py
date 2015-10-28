2106f7a2837b6587eafc87ca946c05c0b8c7a12f
mozhou <mzhou@berkeley.edu>
2015-10-27 22:51:57 -0700

    fix dashboard

--- src/server/opinion/includes/queryutils.py
+++ src/server/opinion/includes/queryutils.py
@@ -2720,7 +2720,7 @@ def get_course_trend(user_set, start_date, is_self):
 
     less_than = 7
     greater_than = 14
-    for i in range(num_weeks-1):
+    for i in range(num_weeks):
         if is_self == 1:
             weekly_data.append(UserRating.objects.filter(user=user_set, created__gte=(start_date+datetime.timedelta(days=less_than)),created__lt=(start_date+datetime.timedelta(days=greater_than)))) # doesnot seem to work!
         else:
