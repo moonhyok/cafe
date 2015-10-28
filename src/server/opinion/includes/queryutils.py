d35ea4677eef98fcccc3bc3b7c4e7aa198b044e6
mozhou <mzhou@berkeley.edu>
2015-10-27 23:59:20 -0700

    fix dashboard

--- src/server/opinion/includes/queryutils.py
+++ src/server/opinion/includes/queryutils.py
@@ -447,8 +447,7 @@ def get_course_trend(user_set, start_date, is_self):
                 to_append = df.groupby('opinion_space_statement')['rating'].mean()[k]
             temps.append(to_append)
         result[k].append(temps)
-    results
-    return result
+    return results
 
 def models_to_array(qs, names):
     """
