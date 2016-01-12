f9f6651bf57bf4e1c37abf7d4dfa76a74b09bb5f
mozhou <mzhou@berkeley.edu>
2016-01-12 23:13:28 +0800

    change to ieor170

--- src/server/opinion/opinion_core/views.py
+++ src/server/opinion/opinion_core/views.py
@@ -182,7 +182,7 @@ def mobile(request,entry_code=None):
     if external_count.count() > 0:
        num_users = external_count[0].value
     # print str(request.user.is_authenticated()) + " authenticated"
-    start_date = datetime.datetime(year=2015, month=8, day=16,hour=0, minute=0, second=0, microsecond=0)
+    start_date = datetime.datetime(year=2016, month=1, day=11,hour=0, minute=0, second=0, microsecond=0)
     end_date = start_date + datetime.timedelta(weeks=17)
    # except:
    #   start_date = os.created
@@ -399,7 +399,7 @@ def mcafe_stats(request):
   #    end_date = os.created + datetime.timedelta(weeks=17)
  #   start_date = datetime.date(year=2015, month=8, day=1)
  #   end_date = start_date + datetime.timedelta(weeks=17)
-    start_date = datetime.datetime(2015,8,17,0,0)
+    start_date = datetime.datetime(2016,1,11,0,0)
     end_date = start_date + datetime.timedelta(weeks=17)
   #  except:
   #    start_date = os.created
